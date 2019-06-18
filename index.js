8078fb7c4410b4cb9c15061ab3de019f8e983b7d
Marcos Henrique <wakeupmh@gmail.com>
2019-06-18 16:36:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const Instagram = require('node-instagram').default;
+const express = require('express');
+
+const instagram = new Instagram({
+  clientId: '22bd2e47d25241b9b62d61cf4f70bf3d',
+  clientSecret: '346f20d4d71a44c1b0d411b7baeba743',
+});
+
+const redirectUri = 'http://localhost:3000/auth/instagram/callback';
+
+const app = express();
+
+app.get('/auth/instagram', (req, res) => {
+  res.redirect(instagram.getAuthorizationUrl(redirectUri, { scope: ['public_content'] }));
+});
+
+app.get('/auth/instagram/callback', async (req, res) => {
+  try {
+    const response = await instagram.authorizeUser(req.query.code, redirectUri);
+    res.json(response);
+    const user = await instagram.get('users/self', { access_token: response.access_token }).then(result => {
+       return {followers: result.data.counts.followed_by, userName: result.data.username, _id: result.data._id, img: result.data.profile_picture};
+    });
+    instagram.get('users/self/media/recent', { access_token: response.access_token }).then(result => {
+        let recentPhoto = result.data[0].id;
+        console.log(recentPhoto);
+        let percent = result.data.map(x=>{
+            return (parseInt(x.likes.count) / parseInt(user.followers)).toFixed(2);
+        });
+        let media = percent.reduce((total, valor) => total+valor/percent.length, 0);
+        let variancia = percent.reduce((total, valor) => total + Math.pow(media - valor, 2)/percent.length, 0);
+        let desvioPadrao = Math.sqrt(variancia);
+
+        console.log(`######## @${user.userName} ##########\n`)
+        console.log(`${(media*100).toFixed(2)}% dos seus seguidores em média curtiram suas fotos`);
+        console.log(`Com uma variância de ${variancia.toFixed(4)} por curtida`);
+        console.log(`E um desvio padrão de ${desvioPadrao.toFixed(4)}\n`);
+
+    });
+  } catch (err) {
+    res.json(err); 
+  }
+});
+
+app.listen(3000, () => {
+  console.log('app listening on http://localhost:3000');
+});
